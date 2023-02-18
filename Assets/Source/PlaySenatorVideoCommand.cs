aee54bac874c0b613f9ce7b80d13ad6ac2b284f9
AtennopVer <anton7185569@gmail.com>
2023-02-18 17:37:42 +0300

    Added first day

---
+++ Assets/Source/PlaySenatorVideoCommand.cs
@@ -0,0 +1,17 @@
+﻿using Naninovel;
+using UnityEngine;
+
+namespace VisualNovel
+{
+    [CommandAlias("playerSenatorVideo")]
+    public class PlaySenatorVideoCommand : Command
+    {
+        private const string SENATOR_VIDEO_URL = "https://www.youtube.com/watch?v=4ZIR1nd60To&ab_channel=SpammingTowers";
+        
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            Application.OpenURL(SENATOR_VIDEO_URL);
+            return UniTask.CompletedTask;
+        }
+    }
+}
